0160f0a9024529c149fd07861a5727a51fb48cf8
Sarah Reichelt <sarah@troz.net>
2020-07-03 14:32:25 +1000

    Initial Commit

---
+++ SwiftUI-Mac-11/ContentView.swift
@@ -0,0 +1,20 @@
+//
+//  ContentView.swift
+//  SwiftUI-Mac-11
+//
+//  Created by Sarah Reichelt on 3/7/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!").padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SwiftUI-Mac-11/SwiftUI_Mac_11App.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_Mac_11App.swift
+//  SwiftUI-Mac-11
+//
+//  Created by Sarah Reichelt on 3/7/20.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_Mac_11App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
